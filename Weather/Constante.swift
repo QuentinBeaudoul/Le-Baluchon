0479b0a7f07225c710fb31bfb1343d62950ea182
Quentin Beaudoul <qbeaudoul@icloud.com>
2022-02-18 11:17:47 +0100

    Constantes weather

---
+++ Weather/Constante.swift
@@ -0,0 +1,13 @@
+//
+//  Constante.swift
+//  Weather
+//
+//  Created by Quentin Beaudoul on 18/02/2022.
+//
+
+import Foundation
+
+class Constante {
+    static let apiKey = "cd93030edd8258fe20f9a2f23c0f1f65"
+    static let weatherUrl = "api.openweathermap.org/data/2.5/weather"
+}
